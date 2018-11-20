a39f4d0128573c2bb837ea49be271c3038e91256
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-11-20 14:19:21 -0500

    more setup

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,62 @@
 var connection = require('../config/connection');
+
+const qMarks = (num) => {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push('?');
+  }
+  return arr.toString();
+};
+
+const objToSql = (ob) => {
+  for (var key in ob) {
+    var value = ob[key];
+
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+        value = `'${value}'`;
+      }
+
+      arr.push(`${key} = ${value}`);
+    }
+  }
+  return arr.toString();
+}
+
+var orm = {
+  all: (table, cb) => {
+    var qString = `SELECT * FROM ${table};`;
+    connection.query(qString, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertData: (table, cols, vals, cb) => {
+    var qString = `INSERT INTO ${table}`;
+    var colsS = cols.toString();
+    var qS = qMarks(vals.length);
+
+    qString += ` (${colsS}) VALUES (${qS}) `
+
+    console.log(qString);
+
+    connection.query(qString, vals, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  update: (table, obColVals, condition, cb) => {
+    var oCV = objToSql(obColVals);
+    var qString = `UPDATE ${table} SET ${oCV} WHERE ${condition}`;
+
+    console.log(qString);
+
+    connection.query(qString, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
