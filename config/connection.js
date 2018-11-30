7da68f66ceff00958e92ae1576951374ffa7ec14
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-11-29 19:44:31 -0500

    ready to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  database: 'pizzasdb'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    database: "pizzasdb"
+  });
+}
 
 connection.connect(err => {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
